bae66cfc2823bfbbfbf2bd90440bfd2e7cbcab78
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-12-09 09:11:53 +0330

    feat(schema): add user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,13 @@
+import { object, string } from 'zod';
+
+export const creatUserSchema = object({
+  body: object({
+    name: string().min(2, 'Name is required'),
+    email: string().email('Invalid email format'),
+    password: string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: string().min(6),
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: 'Password do not match!',
+    path: ['confirmPassword'],
+  }),
+});

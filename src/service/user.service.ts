860eb93e831c29945f7ef97f99bdac05145a7793
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-12-13 16:15:47 +0330

    feat(service): add validatPassword function to user.service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 import UserModel, { UserDocument } from '../models/user.model';
+import { email } from 'zod';
+import { fa } from 'zod/v4/locales';
+import { omit } from 'lodash';
 
 export type UserInput = Omit<
   UserDocument,
@@ -8,8 +11,30 @@ export type UserInput = Omit<
 
 export async function createUser(input: UserInput) {
   try {
-    return await UserModel.create(input);
+    const user = await UserModel.create(input);
+
+    return omit(user.toJSON(), 'password');
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+export async function validatPassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.camparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), 'password');
 }

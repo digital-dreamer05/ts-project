9c2ec08945801533303f67423a59120ae00270b3
digital_dreamer05 <moohammadi.dev@gmail.com>
2025-12-18 11:54:43 +0330

    feat(config): add public and private keys to default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
 export default {
   port: 1337,
   dbUri: 'mongodb://localhost:27017/ts-project',
   saltWorkFactor: 10,
+
+  privateKey: fs.readFileSync(
+    path.join(__dirname, '../keys/private.pem'),
+    'utf8'
+  ),
+
+  publicKey: fs.readFileSync(
+    path.join(__dirname, '../keys/public.pem'),
+    'utf8'
+  ),
 };
